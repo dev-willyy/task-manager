66d179cb3ed0399f9a40a67e5807da66a475bfe0
Madu Williams <97499846+Prodigy97@users.noreply.github.com>
2022-07-06 11:33:12 +0100

    Modified and Finished off coding my Task Manager Web App

---
+++ components/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Toggle NavBar
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
