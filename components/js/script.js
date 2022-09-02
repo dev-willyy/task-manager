ec49918ffdfbc9dba1c87b820e3a0293f6360162
Madu Williams <97499846+Prodigy97@users.noreply.github.com>
2022-09-02 18:33:20 +0100

    Fixed first CSS modal Bug

--- components/js/script.js
+++ components/js/script.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 
 // Toggle NavBar
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const header = document.querySelector("[data-header]");
 
 navToggleBtn.addEventListener("click", function () {
-  header.classList.toggle("active");
+    header.classList.toggle("active");
 });
+
+const closeMenuBar = () => {
+    header.classList.remove("active");
+};
